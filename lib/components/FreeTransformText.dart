71af70b380be5ccef0a7e87252ee9b749754bf0b
Gurkaran Singh <gurkaran@grevity.in>
2021-01-17 06:02:28 +0530

    new changes

--- lib/components/FreeTransformText.dart
+++ lib/components/FreeTransformText.dart
@@ -1,114 +1,74 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:matrix_gesture_detector/matrix_gesture_detector.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
-
+import 'dart:ui' as ui;
 class FreeTransformText extends StatefulWidget {
   @override
   _FreeTransformTextState createState() => _FreeTransformTextState();
 }
 
 class _FreeTransformTextState extends State<FreeTransformText> {
-  Matrix4 matrix = Matrix4.identity();
-  ValueNotifier<int> notifier = ValueNotifier(0);
-  vector.Vector3 nodePosition = vector.Vector3(50, 0, 0);
-  bool check = false;
+  GlobalKey _globalKey = new GlobalKey();
+  Uint8List imageInMemory;
+
+  Future<Uint8List> _capturePng() async {
+    RenderRepaintBoundary boundary = _globalKey.currentContext.findRenderObject();
+
+    if (boundary.debugNeedsPaint) {
+      print("Waiting for boundary to be painted.");
+      await Future.delayed(const Duration(milliseconds: 20));
+      return _capturePng();
+    }
+
+    var image = await boundary.toImage(pixelRatio: 3);
+    var byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    return byteData.buffer.asUint8List();
+  }
+
+  void _printPngBytes() async {
+    var pngBytes = await _capturePng();
+    setState(() {
+      imageInMemory = pngBytes;
+    });
+    var bs64 = base64Encode(pngBytes);
+    print(pngBytes);
+    print(bs64);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Testing Some Shit'),
-      ),
-      body: LayoutBuilder(
-        builder: (ctx, constraints) {
-          return MatrixGestureDetector(
-            shouldRotate: true,
-            onMatrixUpdate: (m, tm, sm, rm) {
-              matrix = MatrixGestureDetector.compose(matrix, tm, sm, rm);
-              notifier.value++;
-            },
-            child: Container(
-              width: double.infinity,
-              height: double.infinity,
-              alignment: Alignment.topLeft,
-              color: Color(0xff444444),
-              child: AnimatedBuilder(
-                animation: notifier,
-                builder: (ctx, child) {
-                  return Container(
-                      width: double.infinity,
-                      height: double.infinity,
-                      child: Stack( // a stack in which all nodes are built
-                        children: <Widget>[
-                          buildNode()
-                        ],
-                      )
-                  );
-                },
+    return RepaintBoundary(
+      key: _globalKey,
+      child: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Widget To Image demo'),
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(
+                'click below given button to capture iamge',
               ),
-            ),
-          );
-        },
+              new RaisedButton(
+                child: Text('capture Image'),
+                onPressed: _printPngBytes,
+              ),
+              imageInMemory != null
+                  ? Container(
+                  child: Image.memory(imageInMemory),
+                  margin: EdgeInsets.all(10))
+                  : Container()
+            ],
+          ),
+        ),
       ),
     );
   }
-
-
-// I made this into a method so the transformed matrix can be calculated at runtime
-  Widget buildNode() {
-    // create a clone of the main matrix and translate it by the node's position
-    Matrix4 ma = matrix.clone();
-    ma.translate(nodePosition.x, nodePosition.y);
-
-    return Transform(
-        transform: ma, // transform the node using the new (translated) matrix
-        child: MatrixGestureDetector(
-            shouldRotate: false,
-            shouldScale: false,
-            onMatrixUpdate: (m, tm, sm, rm) {
-              Matrix4 change = tm;
-              // move the node (in relation to the viewport zoom) when it's being dragged
-              double sc = MatrixGestureDetector.decomposeToValues(matrix).scale;
-              nodePosition += change.getTranslation() / sc;
-              notifier.value++; // refresh view
-            },
-            // design a node holding a bool variable ('check')...
-            child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.blue
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.only(left: 12,right: 12),
-                      decoration: BoxDecoration(
-                          color: Colors.black12,
-                          borderRadius: BorderRadius.all(Radius.circular(8))
-                      ),
-                      child: IntrinsicWidth(
-                          stepWidth: 24,
-                          child: TextField(
-                            maxLines: 8,
-                            minLines: 1,
-                            keyboardType: TextInputType.multiline,
-                            cursorColor: Colors.white,
-                            decoration: InputDecoration(
-                              focusedBorder: InputBorder.none,
-                              enabledBorder: InputBorder.none,
-                              errorBorder: InputBorder.none,
-                              disabledBorder: InputBorder.none,
-                            ),
-                          )
-                      ),
-                    ),
-                  ],
-                ),
-            )
-        )
-    );
-  }
-
 }
 
 
